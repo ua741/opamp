8d4235754103900fcfbbfe64584e6a3f5ba3a270
Neeraj Gupta <neeraj.gupta983@gmail.com>
2011-09-26 22:53:05 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opamp::Application.config.secret_token = '6bb00faf5cb36992da9a4595940f0106d8e877e696f3c7376a0b8a1b646a483275bd74954aaa0ec8c4ff2742a403a911185df93db3d3410fe4ae54ff349cf253'
